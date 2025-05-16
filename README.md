e172b4b47ed8e90d68c15c1ec1f24f6ad92c5fc4
tejas7410 <img_2021030@iiitm.ac.in>
2025-05-17 04:07:34 +0530

    updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Project
+# To know more about me and see my other work, please visit my portfolio website:
 
-This is one of my projects.
-
-To know more about me and see my other work, please visit my portfolio website:
-
-[https://tejaskangule.com](https://tejaskangule.com)
+# [https://tejaskangule.com](https://tejaskangule.com)
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AnimatePresence } from "framer-motion";
 import Index from "./pages/Index";
@@ -18,13 +18,12 @@ const App = () => (
         <Toaster />
         <Sonner />
         <AnimatePresence>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </AnimatePresence>
       </TooltipProvider>
     </ThemeProvider>
